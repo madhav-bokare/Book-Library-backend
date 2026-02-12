1824e68b20e6aef680189c75ef571f308398c880
madhav-bokare <madhavbokare926@gmail.com>
2026-02-12 18:33:18 +0530

    Update connect.js

--- mongoDB/connect.js
+++ mongoDB/connect.js
@@ -10,7 +10,9 @@ export default async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI).then((m) => m);
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI)
+      .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
